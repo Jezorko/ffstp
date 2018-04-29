747788a301b778eb604e777a6903b3c3aa1ddefc
Paweł Szymański <pawel.szymanski@upaid.pl>
2018-04-29 01:41:43 +0100

    Remove all the stuff (this will be a maven repo after all)
    https://gist.github.com/fernandezpablo85/03cf8b0cd2e7d8527063

--- src/main/java/jezorko/ffstp/exception/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains exception classes used by the library.
- * For more information on what exception is thrown when, see {@link jezorko.ffstp.FriendlyForkedSocketTransferProtocolReader#readMessage}.
- */
-package jezorko.ffstp.exception;
